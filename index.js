610da6440ba339a702d5706f7d6ce9866a77fb9b
cooneyasendia <william.cooney@asendia.com>
2020-07-14 18:25:17 -0400

    new app

--- index.js
+++ index.js
@@ -1,89 +1,209 @@
-(() => {
-    const chanceEl = document.getElementById('chance');
-    const daysEl = document.getElementById('days');
-    const rollBtn = document.getElementById('roll');
-    const unknownEl = document.getElementById('unknown');
-    const successEl = document.getElementById('success');
-    const deniedEl = document.getElementById('denied');
-    const addDaysPointsBtn = document.getElementById('addDaysPoints');
-    const addPointsBtn = document.getElementById('addPoints');
-    const addPointsInput = document.getElementById('addPointsInput');
+(function () {
+    'use strict';
 
-    const roll = () => Math.floor(Math.random() * 100) + 1;
-
-    const saveChance = () => localStorage.setItem('denial-chance', window.chance);
-    const loadChance = () => {
-        const result = localStorage.getItem('denial-chance');
-        if(result) {
-            window.chance = result;
-        } else {
-            window.chance = 50;
-            saveChance();
+    var ChanceDailyAddType;
+    (function (ChanceDailyAddType) {
+        ChanceDailyAddType[ChanceDailyAddType["Days"] = 0] = "Days";
+        ChanceDailyAddType[ChanceDailyAddType["ShortVideo"] = 1] = "ShortVideo";
+        ChanceDailyAddType[ChanceDailyAddType["LongVideo"] = 2] = "LongVideo";
+        ChanceDailyAddType[ChanceDailyAddType["DaysAndShortVideo"] = 3] = "DaysAndShortVideo";
+    })(ChanceDailyAddType || (ChanceDailyAddType = {}));
+    var getDailyValueByType = function (type) {
+        switch (type) {
+            case ChanceDailyAddType.Days:
+            case ChanceDailyAddType.ShortVideo:
+                return 5;
+            case ChanceDailyAddType.LongVideo:
+            case ChanceDailyAddType.DaysAndShortVideo:
+                return 10;
         }
-        showChance();
     };
-    const showChance = () => chanceEl.innerHTML = window.chance + '%';
-    const clampChance = () => window.chance = (window.chance > 100 ? 100 : (window.chance < 0 ? 0 : window.chance));
-
-    const rollForRelease = () => {
-        const result = roll() <= window.chance;
-        if(result) {
-            unknownEl.classList.remove('show-it');
-            successEl.classList.add('show-it');
-            deniedEl.classList.remove('show-it');
-        } else {
-            unknownEl.classList.remove('show-it');
-            successEl.classList.remove('show-it');
-            deniedEl.classList.add('show-it');
+    var Chance = /** @class */ (function () {
+        function Chance() {
+            this._value = 50;
+            this.roll = null;
+            this.canRelease = null;
+            var existingChance = localStorage.getItem('chance');
+            if (existingChance) {
+                this._value = +existingChance;
+            }
         }
-        return result;
-    };
-    const handleRelease = () => {
-        window.chance -= 10;
-        clampChance();
-        saveChance();
-    };
+        Object.defineProperty(Chance.prototype, "value", {
+            get: function () {
+                return this._value;
+            },
+            set: function (newValue) {
+                if (newValue < 0) {
+                    this._value = 0;
+                }
+                else if (newValue > 50) {
+                    this._value = 50;
+                }
+                else {
+                    this._value = newValue;
+                }
+                localStorage.setItem('chance', this._value.toString());
+            },
+            enumerable: false,
+            configurable: true
+        });
+        Chance.prototype.forgot = function () {
+            this.value = 0;
+        };
+        Chance.prototype.handleDaily = function (type) {
+            var dailyValue = getDailyValueByType(type);
+            this.value += dailyValue;
+        };
+        Chance.prototype.handleRelease = function () {
+            this.value -= 10;
+        };
+        Chance.prototype.rollForRelease = function () {
+            this.roll = Math.floor(Math.random() * 99) + 1;
+            this.canRelease = this.roll <= this.value;
+            if (this.canRelease) {
+                this.handleRelease();
+            }
+        };
+        return Chance;
+    }());
 
-    const saveDays = () => localStorage.setItem('denial-days', window.days);
-    const loadDays = () => {
-        const result = localStorage.getItem('denial-days');
-        if(result) {
-            window.days = result;
-        } else {
-            window.days = 0;
-            saveDays();
+    var Days = /** @class */ (function () {
+        function Days() {
+            this._days = 0;
+            this._consecutiveDays = 0;
+            var existingDays = localStorage.getItem('days');
+            if (existingDays) {
+                this._days = +existingDays;
+            }
+            var existingConsecutiveDays = localStorage.getItem('consecutive_days');
+            if (existingConsecutiveDays) {
+                this._consecutiveDays = +existingConsecutiveDays;
+            }
         }
-        showDays();
-    };
-    const showDays = () => daysEl.innerHTML = window.days;
-    const handleDays = () => {
-        window.days++;
-    };
+        Object.defineProperty(Days.prototype, "days", {
+            get: function () {
+                return this._days;
+            },
+            set: function (newDays) {
+                this._days = newDays;
+                localStorage.setItem('days', this._days.toString());
+            },
+            enumerable: false,
+            configurable: true
+        });
+        Object.defineProperty(Days.prototype, "consecutiveDays", {
+            get: function () {
+                return this._consecutiveDays;
+            },
+            set: function (newConsecutiveDays) {
+                this._consecutiveDays = newConsecutiveDays;
+                localStorage.setItem('consecutive_days', this._consecutiveDays.toString());
+            },
+            enumerable: false,
+            configurable: true
+        });
+        Days.prototype.add = function (isConsecutive) {
+            this.days++;
+            if (isConsecutive) {
+                this.consecutiveDays++;
+            }
+            else {
+                this.consecutiveDays = 0;
+            }
+        };
+        Days.prototype.canUseDays = function () {
+            return this.days >= 7 || this.consecutiveDays >= 5;
+        };
+        Days.prototype.resetDays = function () {
+            this.days = 0;
+            this.consecutiveDays = 0;
+        };
+        return Days;
+    }());
 
-    loadChance();
-    loadDays();
-    rollBtn.addEventListener('click', () => {
-        const result = rollForRelease();
-        if(result) {
-            handleRelease();
+    var chance = new Chance();
+    var days = new Days();
+    var btnRoll = document.getElementById('btnRoll');
+    var btnAddDay = document.getElementById('btnAddDay');
+    var btnAddConsecutiveDay = document.getElementById('btnAddConsecutiveDay');
+    var btnUseDays = document.getElementById('btnUseDays');
+    var btnVideoShort = document.getElementById('btnVideoShort');
+    var btnVideoLong = document.getElementById('btnVideoLong');
+    var btnBoth = document.getElementById('btnBoth');
+    var btnForgot = document.getElementById('btnForgot');
+    var bodyEl = document.getElementById('body');
+    var chanceEl = document.getElementById('chance');
+    var canReleaseEl = document.getElementById('canRelease');
+    var daysEl = document.getElementById('days');
+    var consecutiveDaysEl = document.getElementById('consecutiveDays');
+    var hrUseDaysEl = document.getElementById('hrUseDays');
+    var updateScreen = function () {
+        chanceEl.innerHTML = chance.value.toFixed(0) + "%";
+        daysEl.innerHTML = days.days.toString();
+        consecutiveDaysEl.innerHTML = days.consecutiveDays.toString();
+        if (chance.canRelease !== null) {
+            if (chance.canRelease) {
+                bodyEl.classList.remove('bg-red');
+                bodyEl.classList.add('bg-green');
+            }
+            else {
+                bodyEl.classList.add('bg-red');
+                bodyEl.classList.remove('bg-green');
+            }
         }
-        handleDays();
-        showChance();
-        showDays();
+        if (days.canUseDays()) {
+            btnUseDays.classList.remove('hidden');
+            btnUseDays.classList.add('inline');
+            btnBoth.classList.remove('hidden');
+            btnBoth.classList.add('inline');
+            hrUseDaysEl.classList.remove('hidden');
+            hrUseDaysEl.classList.add('inline');
+        }
+        else {
+            btnUseDays.classList.add('hidden');
+            btnUseDays.classList.remove('inline');
+            btnBoth.classList.add('hidden');
+            btnBoth.classList.remove('inline');
+            hrUseDaysEl.classList.add('hidden');
+            hrUseDaysEl.classList.remove('inline');
+        }
+    };
+    btnRoll.addEventListener('click', function () {
+        var canRelease = chance.rollForRelease();
+        canReleaseEl.innerHTML = chance.canRelease ? 'YES!' : 'NO!';
+        updateScreen();
     });
-    addDaysPointsBtn.addEventListener('click', () => {
-        window.days = 0;
-        saveDays();
-        showDays();
-        window.chance += 5;
-        clampChance();
-        saveChance();
-        showChance();
+    btnAddDay.addEventListener('click', function () {
+        days.add(false);
+        updateScreen();
     });
-    addPointsBtn.addEventListener('click', () => {
-        window.chance += Number(addPointsInput.value);
-        clampChance();
-        saveChance();
-        showChance();
+    btnAddConsecutiveDay.addEventListener('click', function () {
+        days.add(true);
+        updateScreen();
     });
-})();
+    btnUseDays.addEventListener('click', function () {
+        days.resetDays();
+        chance.handleDaily(ChanceDailyAddType.Days);
+        updateScreen();
+    });
+    btnVideoShort.addEventListener('click', function () {
+        chance.handleDaily(ChanceDailyAddType.ShortVideo);
+        updateScreen();
+    });
+    btnVideoLong.addEventListener('click', function () {
+        chance.handleDaily(ChanceDailyAddType.LongVideo);
+        updateScreen();
+    });
+    btnBoth.addEventListener('click', function () {
+        chance.handleDaily(ChanceDailyAddType.DaysAndShortVideo);
+        updateScreen();
+    });
+    btnForgot.addEventListener('click', function () {
+        chance.forgot();
+        days.resetDays();
+        updateScreen();
+    });
+    updateScreen();
+
+}());
+//# sourceMappingURL=index.js.map
